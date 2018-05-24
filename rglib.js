c8eebc0cd755d594882177897864e4914b5b29c7
Gregory N. Schmit <schmitgreg@gmail.com>
2018-05-24 09:03:54 -0500

    keep empty array in define

--- rglib.js
+++ rglib.js
@@ -6,7 +6,7 @@
 (function (root, factory) {
   if(typeof define === "function" && define.amd) {
     // requirejs/AMD stuff
-    define(function(){
+    define([], function(){
       return (root.rglib = factory());
     });
   } else if(typeof module === "object" && module.exports) {
